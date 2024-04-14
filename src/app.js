cc6253e7388da88833251660f996437aac00956a
Nico <nicolas2289h@gmail.com>
2024-04-14 17:26:29 -0300

    corrigiendo pruebas de deploy

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const authRouter = require('./routes/auth.routes.js');
 const reservaRouter = require('./routes/reserva.routes.js')
-const db = require('./db.js')
 
 const app = express()
 
@@ -17,26 +16,6 @@ app.use(bodyParser.json());
 // Rutas
 app.use(authRouter)
 app.use(reservaRouter)
-
-// BORRAR PRUEBA1
-app.get('/home', (req, res) => {
-    res.json('home')
-})
-
-// BORRAR PRUEBA2
-app.post('/guardar', (req, res) => {
-    const { nombre, apellido } = req.body
-    console.log(req.body)
-    const query = `INSERT INTO prueba (nombre, apellido) VALUES (?,?)`
-    db.query(query, [nombre, apellido], (err, result) => {
-        if (err) {
-            console.error('Error en test:', err);
-            return res.status(500).json({ message: 'Error al guardar test: ' + err })
-        }
-
-        return res.status(201).json({ message: 'test guardada.' })
-    })
-})
 
 const port = process.env.PORT || 3000
 
